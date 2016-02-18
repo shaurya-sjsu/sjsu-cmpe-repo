888e6382694152d05a06cc76c51b4ed9421e7289
Shaurya Sharma <shaurya104@gmail.com>
2016-02-18 00:51:41 -0800

    cache Q2
    completed question 2 for lab 1

--- lab1/cache.go
+++ lab1/cache.go
@@ -1,13 +1,40 @@
 package main
 
+import (
+	"container/list"
+	//"fmt"
+)
 // DO NOT CHANGE THIS CACHE SIZE VALUE
 const CACHE_SIZE int = 3
 
-func Set(key int, value int) {
-	// TODO: add your code here!
-}
+var l *list.List = list.New()
+var keyMap = make(map[int]*list.Element)
 
 func Get(key int) int {
-	// TODO: add your code here!
+	v := keyMap[key]
+	if v != nil {
+		l.MoveToFront(v)
+		return v.Value.(int)
+	}
 	return -1
+}
+
+func Set(key int, value int) {
+	v := keyMap[key]
+	if v == nil {
+		if len(keyMap) >= CACHE_SIZE {
+			for v := range keyMap {
+				if keyMap[v].Value == l.Back().Value {
+					delete(keyMap, v)
+					//fmt.Println(l)
+					l.Remove(l.Back())
+					break
+				}
+			}
+		}
+		l.PushFront(value)
+		keyMap[key] = l.Front()
+	} else {
+		l.MoveToFront(v)
+	}
 }
