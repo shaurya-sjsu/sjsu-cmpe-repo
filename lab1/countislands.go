1224e9f8abe4b0f7ada9f26a6e4134b23e3bef52
Shaurya Sharma <shaurya104@gmail.com>
2016-02-17 19:15:52 -0800

    Question 1 code
    added answer code to check for islands in a 2d array

--- lab1/countislands.go
+++ lab1/countislands.go
@@ -1,6 +1,50 @@
 package main
 
+import (
+	"container/list"
+)
+
 func CountIslands(grid [][]int) int {
-	// TODO: add your code here!
-	return 0
+	rows := len(grid)
+	columns := len(grid[0])
+	var visited = make([][]int, rows)
+	for i := range visited {
+		visited[i] = make([]int, columns)
+	}
+	//fmt.Println(visited)
+	var i, j, ti, tj, is, temp int
+	is = 0
+	for i = 0; i < rows; i++ {
+		for j = 0; j < columns; j++ {
+			if grid[i][j] == 1 && visited[i][j] != 1 {
+				visited[i][j] = 1
+				is = is + 1
+				l := list.New()
+
+				l.PushBack(10*i + j)
+
+				for e := l.Front(); e != nil; e = e.Next() {
+					temp = (e.Value).(int)
+					ti = temp / 10
+					tj = temp % 10
+					visited[ti][tj] = 1
+
+					if ti != 0 && grid[ti-1][tj] == 1 && visited[ti-1][tj] != 1 {
+						l.PushBack(10*(ti-1) + tj)
+					}
+					if tj != 0 && grid[ti][tj-1] == 1 && visited[ti][tj-1] != 1 {
+						l.PushBack(10*ti + (tj - 1))
+					}
+					if ti != (rows-1) && grid[ti+1][tj] == 1 && visited[ti+1][tj] != 1 {
+						l.PushBack(10*(ti+1) + tj)
+					}
+					if tj != (columns-1) && grid[ti][tj+1] == 1 && visited[ti][tj+1] != 1 {
+						l.PushBack(10*ti + (tj + 1))
+					}
+
+				}
+			}
+		}
+	}
+	return is
 }
